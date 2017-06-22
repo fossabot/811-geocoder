8d414b81e3462eafcfa6e33dbe743450e4124ce9
Stephonovich <stephonovich@gmail.com>
2017-06-22 10:16:02 -0400

    Update Geocoder.py

--- Geocoder.py
+++ Geocoder.py
@@ -4,23 +4,23 @@
 # Locate Geocoder with KML writing                                                   #
 # Feed in a CSV, receive KML Long/Lat                                                #
 # Author: Stephan Garland, Carroll White REMC                                        #
-# Shoutout to Wiktor Stribiżew on StackOverflow for the regex                        #
+# Shoutout to Geocod.io for having the best address normalizer I have ever seen      #
 # Directions: Create a CSV that has a header named Address, containing the address   #
-#             The regex is setup for USIC's naming scheme for addresses:             #
-#             $HOUSE_NUM $STREET_NAME $TOWN $NEAREST_INTERSECTION                    #
-#             $INTERSECTING_STREET_1_NAME $TOWN $INTERSECTING_STREET_2_NAME          #
+#                                                                                    #
 #             Run the script, import the KML into ArcEarth with your territory KML   #
-#             TODO: Point-in-polygon, maybe                                          #
 ######################################################################################
 
 import csv
-import re
+import json
 import time
-import geocoder
 import simplekml
+import shapely
+import string
 
 # For quick and easy GUI, I'm informing the user with pymsgbox, and getting files with tkinter
 import pymsgbox.native as gui
+
+import pandas as pd
 
 try:
     from tkinter import Tk as tk  # Python 3.x
@@ -32,8 +32,13 @@ except ImportError:  # Python 2.x
     from tkFileDialog import askopenfilename as getFile
     from tkFileDialog import asksaveasfilename as saveFile
 
+from geocodio import GeocodioClient
+from shapely.geometry import Point, shape
+from os import remove
 from sys import version_info
 from time import strftime
+from xlsxwriter.utility import xl_rowcol_to_cell
+
 
 
 def hms_string(sec_elapsed):
@@ -43,6 +48,7 @@ def hms_string(sec_elapsed):
     return "{}:{:>02}:{:>05.2f}".format(h, m, s)
 
 
+
 def printToLog(inputText):
     textToPrint = inputText
     print(textToPrint)
@@ -50,57 +56,148 @@ def printToLog(inputText):
         log.write(textToPrint)
 
 
+
 # Does a simple bounding check to see if the plotted point is roughly in our area
-# Change the coordinates to whatever area you need, or comment it out
 def boundsCheck(x, y):
     failString = "\n***** " + kmlAddr + " failed sanity check, manually check territory *****"
     # Negative check because otherwise the positive matches wouldn't get printed
-    if not (abs(floatGeoLat) > 41.07 or abs(floatGeoLong) > 87.152 or abs(floatGeoLat) < 40.418 or abs(floatGeoLong) < 86.18):
+    if not (abs(floatGeoLat) > geoLatMax or abs(floatGeoLong) > geoLongMax or abs(floatGeoLat) < geoLatMin or abs(floatGeoLong) < geoLongMin):
         print("\n" + kmlAddr + " correctly mapped.")
+        # Moved the plotting into the function so out-of-bounds are only logged in the text file
+        kml.newpoint(name = kmlAddr, coords = [reverseGeo])
     else:
         printToLog(failString)
+
 
 
 # Allows user to quit at any time throughout GUI input
 def killProgram(cancel):
     if cancelSel == 'Cancel':
-        print("User quit!")
-        time.sleep(3)
+        gui.alert("Goodbye!")
         quit()
     else:
         pass
-
         
-cancelSel = gui.confirm("Please enter your organization's name ", "Organization", ['OK', 'Cancel'])
-killProgram(cancelSel)
 
-try:
-    orgName = raw_input("Organization: ")  # Python 2.x
-except NameError:
-    orgName = input("Organization: ")  # Python 3.x
 
-cancelSel = gui.confirm("Please enter the two-digit code for your state", "Select state", ['OK', 'Cancel'])
-killProgram(cancelSel)
+def getInput(question, type):
+    while True:
+        try:
+            return type(gui.prompt(question))
+        except Exception: # Loop if they don't input correct type
+            pass
+        else:
+            gui.alert("Goodbye!")
+            quit()
+            
+   
 
-try:
-    stateVar = raw_input("State: ").upper()  # Python 2.x
-except NameError:
-    stateVar = input("State: ").upper()  # Python 3.x
+def jsonRead(input):
+    global jsonFile
+    point = Point(reverseGeo)
+    with open(jsonFile, 'r') as jsonFile:
+        js = json.load(jsonFile)
+    
+    for feature in js['features']:
+            polygon = shape(feature['geometry'])
+            territoryOwner = str(feature['properties']['NAME_ABREV'])
+            if (polygon.contains(point)) \
+            and str(feature['properties']['NAME_ABREV'] == "CWREMC"):       
+                print(str(kmlAddr) + " is in our territory. ID is " + str(feature['properties']['OBJECTID']))
+            else:
+                print(str(kmlAddr) + " is not in our territory; owned by " + territoryOwner)
+                
+                with open(inCSV, 'r') as inFile:
+                    reader = csv.DictReader(inFile, delimiter = ',', quotechar = '"')
+               
+                    with open(outCSV, 'w') as out:
+                        fieldnames = ['Request No', 'Address', 'Owner']
+                        writer = csv.DictWriter(out, delimiter = ',', quotechar = '"', fieldnames = reader.fieldnames)
+                        writer.writeheader()
+                        for row in reader:
+                            if row == "request no":
+                                writer.writerow(row)
+                                writer.writerow({'Request No': row['request no'], 'Address': row['address'], 'Owner': "territoryOwner"})
+                
 
-cancelSel = gui.confirm("Please select your CSV", "CSV Location", ['OK', 'Cancel'])
-killProgram(cancelSel)
+
+def pandasOutput(input):
+    df = pd.read_csv(outCSV)
+    header = ['Request No', 'Address', 'Owner']
+
+    writer = pd.ExcelWriter(outExcel, engine = 'xlsxwriter')
+
+    df.to_excel(writer, index = False, sheet_name = 'Sheet1', columns = header)
+
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+
+    worksheet.set_column('A:A', 15)
+    worksheet.set_column('B:B', 45)
+    worksheet.set_column('C:C', 15)
+
+    writer.save()
+        
+    os.remove(outCSV)
+
+
+
+apiKey = getInput("Please enter your Geocodio API Key", str)
+gc = GeocodioClient(apiKey)
+
+outCSV = 'P:/Out_CSV.csv'
+outExcel = 'P:/Test_Output.xlsx'
 
 root = tk()
 # Hides the TK background window
 root.withdraw()
 
+cancelSel = gui.confirm("Please select your Territory GeoJSON file", "GeoJSON Location", ['OK', 'Cancel'])
+killProgram(cancelSel)
+
+# Select the territory GeoJSON file
+if version_info.major == 2:
+# Python 2.x wants the default as the last item. Python 3.x wants it as the first.
+    jsonFile = getFile(title = "Territory Location",
+                      filetypes = (("All Files", "*.*"),("GeoJSON", "*.geojson")))
+else:
+    jsonFile = getFile(title = "Territory Location",
+                      filetypes = (("GeoJSON", "*.geojson"),("All Files", "*.*")))
+
+try:
+    # pymsgbox literally interprets indentation, hence the non-indented continue line
+    cancelSel = gui.confirm("Enter your rough territory coordinates in unsigned decimal \
+format, e.g. 87.152. Use at least four significant figures.", "Coordinate entry", ['OK', 'Cancel']) 
+    killProgram(cancelSel)
+    # absolute value in case instructions are ignored
+    geoLatMin = abs(float(getInput("Please enter your southernmost point", float)))
+    geoLatMax = abs(float(getInput("Please enter your northernmost point", float)))
+    geoLongMin = abs(float(getInput("Please enter your easternmost point", float)))
+    geoLongMax = abs(float(getInput("Please enter your westernmost point", float)))
+except AttributeError:
+    gui.alert("Goodbye!")
+    quit()
+
+orgName = getInput("Please enter your organization's name", str)
+
+# upper() probably isn't required for Geocodio, but the rest of the address is all caps, so...
+stateVar = getInput("Please enter the two-digit code for your state", str).upper()
+# checks for length of two, and also that input is in upper/lowercase ascii letter set
+while not len(stateVar) == 2 or len(sorted(set(stateVar).difference(string.ascii_letters))) > 0: 
+    stateVar = getInput("Please enter the two-digit code for your state", str).upper()
+else:
+    pass
+
+cancelSel = gui.confirm("Please select your CSV", "CSV Location", ['OK', 'Cancel'])
+killProgram(cancelSel)
+
 # Select the input CSV file
 if version_info.major == 2:
 # Python 2.x wants the default as the last item. Python 3.x wants it as the first.
-    csvFile = getFile(title = "CSV Location",
+    inCSV = getFile(title = "CSV Location",
                       filetypes = (("All Files", "*.*"),("CSV", "*.csv")))
 else:
-    csvFile = getFile(title = "CSV Location",
+    inCSV = getFile(title = "CSV Location",
                       filetypes = (("CSV", "*.csv"),("All Files", "*.*")))
 
 cancelSel = gui.confirm("Please select the logfile save location and filename",
@@ -152,74 +249,35 @@ except ImportError:
 start_time = time.time()
 printToLog("Start: " + strftime("%Y-%m-%d %H:%M:%S") + "\n")
 
-with open(csvFile,'r') as listFile:
-    reader = csv.DictReader(listFile, delimiter = ',', quotechar = '"')
+with open(inCSV,'r') as inFile:
+    reader = csv.DictReader(inFile, delimiter = ',', quotechar = '"')
+    
+    for row in reader:    
+        # Cat address and defined state for geocoding
+        catAddr = row['address'] + " " + stateVar
+        kmlAddr = row['address'] # used for printing to fail log
+        
+        # Run above list through Geocodio, return coords in Lat/Long
+        geoResult = gc.geocode(catAddr).coords
+        
+        # I had to either append [::-1] to the above to reverse it inline, or do so later.
+        # I need to cut out the Long/Lat in the next two for boundary checks.
+        # I decided this way was easier to read, albeit not as compact.
+        reverseGeo = geoResult[::-1]
+        
+        # Slice the string into two parts, cast it to a string, then to a float to compare.
+        # Yes, this is horrendously ugly.         
+        floatGeoLong = float(str(reverseGeo[1:]).replace('(','').replace(')','').replace(',',''))
+        floatGeoLat = float(str(reverseGeo[:1]).replace('(','').replace(')','').replace(',',''))
 
-    for row in reader:
-        testAddr = row['Address']
-        if not testAddr[:1].isdigit():
-            # If the address doesn't start with a number, it's an intersection.
-            # Regex: \s+ - whitespace character[1-inf, greedy]; \S+ - non-whitespace character[1-inf, greedy].
-            #        \s* - whitespace character[0-inf, greedy]; (.*) - any character.
+        # Simple sanity check for service territory; if valid, plots location
+        boundsCheck(floatGeoLong, floatGeoLat)
+        # Feeds each geocoded spot into automated checker
+        jsonRead(reverseGeo)
 
-            # Pull the non-parsed address out, which will include the city.
-            # Note, breaking line after pipe will cause regex to fail (e.g. Street|\).
-            readAddr = re.compile(\
-                                  r"(Avenue|Lane|Road|Court|Boulevard|Drive|Street\
-                                  |Ave|Dr|Rd|Ct|Blvd|Ln|St)\s+(\S+)\s*(.*)", re.I)
-            subAddr = row['Address']
-
-            # Run it through the regex, group it as 1 & 3 2, which generates an intersection and city
-            fixedAddr = re.sub(readAddr, r'\1 & \3 \2', subAddr)
-            
-            # For tagging the map point, ignore the city to save space on map
-            kmlAddr = re.sub(readAddr, r'\1 & \3', subAddr)
-            
-            # Cat address and defined state for geocoding
-            catAddr = fixedAddr + " " + stateVar
-            
-            # Run above list through ArcGIS via geocoder, and save as Lat/Long
-            geoResult = geocoder.arcgis(catAddr).latlng
-            
-            # I had to either append [::-1] to the above to reverse it inline, or do so later.
-            # I need to cut out the Long/Lat in the next two for boundary checks.
-            # I decided this way was easier to read, albeit not as compact.
-
-            # Slice the string into two parts, cast it to a string, then to a float to compare.
-            # Yes, this is horrendously ugly.         
-            floatGeoLong = float(str(geoResult[1:]).replace('[','').replace(']',''))
-            floatGeoLat = float(str(geoResult[:1]).replace('[','').replace(']',''))
-
-            # Simple sanity check for service territory
-            boundsCheck(floatGeoLong, floatGeoLat)
-
-            # simplekml needs Long/Lat, and reversed() wasn't working, so slice the list
-            reverseGeo = geoResult[::-1]
-
-            kml.newpoint(name=kmlAddr, coords=[reverseGeo])
-        else:
-            # Otherwise, assume it has a street number
-            readAddr = re.compile(\
-                                  r"(Avenue|Lane|Road|Court|Boulevard|Drive|Street\
-                                  |Ave|Dr|Rd|Ct|Blvd|Ln|St)\s+(\S+)\s*(.*)", re.I)
-            subAddr = row['Address']
-
-            # For addresses with a house number, there is an nearest-intersecting street in group 3 we ignore
-            fixedAddr = re.sub(readAddr, r'\1 \2', subAddr)
-            kmlAddr = re.sub(readAddr, r'\1', subAddr)
-
-            catAddr = fixedAddr + " " + stateVar
-
-            geoResult = geocoder.arcgis(catAddr).latlng
-
-            floatGeoLong = float(str(geoResult[1:]).replace('[','').replace(']',''))
-            floatGeoLat = float(str(geoResult[:1]).replace('[','').replace(']',''))
-
-            boundsCheck(floatGeoLong, floatGeoLat)
-            reverseGeo = geoResult[::-1]
-
-            kml.newpoint(name = kmlAddr, coords = [reverseGeo])
-
+# Takes jsonInput()'s CSV file (points not owned), generates an Excel file via pandas, deletes CSV
+pandasOutput(outCSV)
+        
 end_time = time.time()
 printToLog("\n\nEnd: " + strftime("%Y-%m-%d %H:%M:%S"))
 printToLog("\n\nIt took {} to execute this script.".format(hms_string(end_time - start_time)))
